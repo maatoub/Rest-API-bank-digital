715663dd04baea1b6bdce160cb9ced0d2431faa8
maatoub <abdenasser.hasnaoui@edu.uiz.ac.ma>
2024-01-19 18:03:26 +0100

    add docker file

--- src/main/java/com/nsr/digitalbanking/config/WebConfig.java
+++ src/main/java/com/nsr/digitalbanking/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/") // Remplacez par l'URL de votre application React
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
